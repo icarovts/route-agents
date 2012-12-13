d5380195833091313aaef1adddb91e9c5a6675d2
Felipe Volcov Nambara <fpfernandesnambara@gmail.com>
2012-12-13 21:56:29 -0200

    .

---
+++ src/routeagents/CarBehavior.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package routeagents;
+
+import jade.core.behaviours.SimpleBehaviour;
+
+/**
+ *
+ * @author Felipe V Nambara
+ */
+public class CarBehavior extends SimpleBehaviour{
+    boolean finished = false;
+    Car car;
+    
+    public CarBehavior(Car car) {
+        
+        super();
+            
+        this.car = car;
+        
+    }
+        
+    @Override
+    public void action() {
+              
+        this.car.startWay();
+
+        
+    }
+
+    @Override
+    public boolean done() {
+        return this.finished;
+    }       
+    
+}
